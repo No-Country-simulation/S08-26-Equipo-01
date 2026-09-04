2662dd744d1739e35d3d1de3f30e0c1cb510af2b
judith2x <judivm86@gmail.com>
2026-09-04 01:04:36 -0600

    QualityTrack

---
+++ src/main/java/com/example/QualityTrack/QualityTrack/QualityTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.QualityTrack.QualityTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityTrackApplication.class, args);
+	}
+
+}
